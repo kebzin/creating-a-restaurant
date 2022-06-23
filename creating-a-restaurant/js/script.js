81ee7eb2fae6a86826045438057d6cfea0c5c3e2
kebzin <honorablewaiga@gmail.com>
2022-06-23 13:44:46 -0700

    first commit

---
+++ creating-a-restaurant/js/script.js
@@ -0,0 +1,15 @@
+let searchform = document.querySelector('.search-form-container');
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+}
+
+
+
+let loginform = document.querySelector('.login-form-container');
+document.querySelector('#login-btn').onclick = () =>{
+    document.loginform.classList.toggle('active')
+}
+
+
+
+
